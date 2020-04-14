eaeef8bf8d1ca81fd5717c43bed685c01f770d44
k sudheer <ksudheer1@outlook.com>
2020-04-14 13:54:22 +0530

    Add project files.

---
+++ SamplePluginCreate/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplePluginCreate
+{
+    public class Class1
+    {
+
+    }
+}
